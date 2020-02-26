7c021e00d99b6bf9cc1539addcea12091b6b9839
Jason <37859597+zachowj@users.noreply.github.com>
2020-02-26 00:00:48 +0000

    Version bump

--- custom_components/nodered/const.py
+++ custom_components/nodered/const.py
@@ -2,7 +2,7 @@
 # Base component constants
 DOMAIN = "nodered"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 REQUIRED_FILES = [
     ".translations/en.json",
     "binary_sensor.py",
