8fb3731b1b2cb3fba5e556fca7ff2d8e97e6225d
Jason <37859597+zachowj@users.noreply.github.com>
2021-12-03 12:11:25 -0800

    chore: Update version number

--- custom_components/nodered/const.py
+++ custom_components/nodered/const.py
@@ -2,7 +2,7 @@
 # Base component constants
 DOMAIN = "nodered"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "0.5.2"
+VERSION = "0.5.4"
 
 ISSUE_URL = "https://github.com/zachowj/hass-node-red/issues"
 
