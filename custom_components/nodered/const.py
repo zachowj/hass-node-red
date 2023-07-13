03ca7850a12d3cab5b8db55f01f8b7e9e85b866b
Jason <37859597+zachowj@users.noreply.github.com>
2023-07-13 16:17:04 +0000

    chore!: Home Assistant version 2023.7+ required
    chore: Update version

--- custom_components/nodered/const.py
+++ custom_components/nodered/const.py
@@ -2,7 +2,7 @@
 # Base component constants
 DOMAIN = "nodered"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "1.7.0"
+VERSION = "2.0.0"
 
 ISSUE_URL = "https://github.com/zachowj/hass-node-red/issues"
 
